1575dafff29f67136eed0e846791f91bcad5a8f4
bundgaard1 <mathiasbbjorgensen@gmail.com>
2022-09-28 14:51:21 +0200

    main.c changes

--- main.c
+++ main.c
@@ -9,7 +9,6 @@ int main(void) {
   /*testing circles - make sure that you know how it works and what it is
    * supposed to print out*/
 
-  /*
   circle c[5];
   fiveCircles(c);
   for (int i = 0; i < 5; i++) {
@@ -20,16 +19,16 @@ int main(void) {
   p.y = 2;
 
   translate(&c[1], &p);
-  printf("You are here \n");
   printCircle(c[1]);
   printf("isValid: %d \n", circleIsValid(&c[1]));
-  */
 
+  printf("-------------------------------------------\n");
+  printf("Input Sequence for test of Jolly Jumper\n");
   /*answer to exercise 7.b*/
   int n; /*number of numbers to read*/
   /*readin n and check that is is OK*/
   scanf("%d", &n);
-  assert(n > 0);
+  assert(n > 0 && n < 100);
 
   int *numbers = malloc(sizeof(int) * n); /*the numbers read*/
 
